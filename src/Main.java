cfebc116c140fb12ba7f22b6c88a958acfca073e
Egoldenberg <egoldenberg25@scienceleadership.org>
2024-09-09 10:03:38 -0400

    Ezra is here

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi class!");`
+        System.out.println("Hi class!");
         System.out.println(".");
-
+        System.out.println("Ezra Goldenberg is here");
 
 
         /*
