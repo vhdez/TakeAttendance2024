fe15bfd980d4e33cc50a40ca74ffb8d6e8a0a9ad
vhdez <vhernandez@scienceleadership.org>
2024-09-09 09:34:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi class!");`
+        System.out.println(".");
+
+
+
+        /*
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        boolean stillAsk = true;
+        while (stillAsk) {
+            System.out.println("Please type your name to show that you are present:");
+
+            String student = myObj.nextLine();  // Read user input
+
+            if (student.equals("DONE")) {
+                stillAsk = false;
+            } else {
+
+                System.out.println(student + " is present");  // Output user input
+            }
+
+        }
+*/
+    }
+}
